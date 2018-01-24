b9540112f24c657033cafbe01ab60895cd2e871c
Vykstorm <victorruizgomezdev@gmail.com>
2018-01-24 11:55:15 +0100

    Creado script con variables que definen constraints del robot epuck

---
+++ epuck_constraints.py
@@ -0,0 +1,18 @@
+
+'''
+Este script define algunas propiedades físicas del robot e-puck.
+'''
+
+# Diámetro del cuerpo del robot en m
+body_diameter = 7 / 100
+
+# Diámetro de las ruedas en m
+wheels_diameter = 4.1 / 100
+
+body_radius = body_diameter / 2
+wheels_radius = wheels_diameter / 2
+
+# Máxima velocidad ángular de los motores en radianes / segundo
+from math import pi
+max_motor_speed = 4 * pi
+
