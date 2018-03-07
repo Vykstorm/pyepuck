098427ec69e3340752ee60bd899e6e4e1f99737d
Vykstorm <victorruizgomezdev@gmail.com>
2018-03-07 16:46:08 +0100

    Creado fichero setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name = 'pyepuck',
+    version = '1.0.0',
+    description = 'Python library and utilities for e-puck robot',
+    url = 'https://github.com/Vykstorm/pyepuck',
+    author = 'Vykstorm',
+    author_email = 'victorruizgomezdev@gmail.com',
+    license = 'MIT',
+    zip_safe = False,
+    packages = [''],
+    install_requires = ['numpy', 'Pillow', 'PyBluez'],
+    include_package_data = True
+)
