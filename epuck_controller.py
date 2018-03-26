8f65c90deff538b377074df9cc16d17cc1fb30dc
Vykstorm <victorruizgomezdev@gmail.com>
2018-03-26 16:59:12 +0200

    Añadidos métodos para controlar el número de pasos por segundo del controlador y para medir el tiempo usado en cada fase de cada iteración del bucle principal del mismo

--- epuck_controller.py
+++ epuck_controller.py
@@ -1,4 +1,6 @@
 
+from time import sleep
+from time import clock
 
 class EPuckController:
     '''
@@ -11,6 +13,18 @@ class EPuckController:
         '''
         self.epuck = epuck
 
+        # Esta variable medirá el tiempo que lleva activa la simulación (se actualiza en cada iteración del bucle)
+        self._elapsed_time = 0
+
+        # Esta variable configura el número de veces que el bucle principal debe ejecutarse por unidad de tiempo.
+        # El número de veces que el bucle principal se ejecutará por segundo es inferior o igual a esta cantidad
+        self.sps = 2
+
+        self._think_times = []
+        self._update_times = []
+        self._think_time = 0
+        self._update_time = 0
+
     def run(self):
         '''
         Lanza el controlador. Inicializa el robot y el controlador, ejecuta el método step() de esta misma clase
@@ -22,7 +36,15 @@ class EPuckController:
             self.init()
             try:
                 while True:
+                    t0 = clock()
                     self.step()
+                    t1 = clock()
+                    step_time = t1 - t0
+                    inv_sps = 1 / self.sps
+                    if step_time < inv_sps:
+                        sleep(inv_sps)
+
+                    self._elapsed_time += inv_sps
             except StopIteration:
                 pass
             finally:
@@ -37,10 +59,30 @@ class EPuckController:
         - Fase de actuación: Se llamada al método act()
         :return:
         '''
+        t0 = clock()
         self.epuck.update()
+        t1 = clock()
+        update_time = t1 - t0
+
         self.sense()
+
+        t0 = clock()
         self.think()
+        t1 = clock()
+        think_time = t1 - t0
+
         self.act()
+
+
+        self._think_times.append(think_time)
+        if len(self._think_times) > 3:
+            self._think_times.pop(0)
+        self._think_time = sum(self._think_times) / 3
+
+        self._update_times.append(think_time)
+        if len(self._update_times) > 3:
+            self._update_times.pop(0)
+        self._update_time = sum(self._update_times) / 3
 
 
     def init(self):
@@ -83,3 +125,34 @@ class EPuckController:
         :return:
         '''
         pass
+
+
+
+    @property
+    def elapsed_time(self):
+        '''
+        Esta propiedad indica el tiempo total transcurrido desde que se comenzó con la ejecución del controlador.
+        :return:
+        '''
+        return self._elapsed_time
+
+    @property
+    def think_time(self):
+        '''
+        Esta propiedad devuelve una estimación del tiempo necesario para ejecutar el método think() de este
+        controlador (se devuelve una media de las últimas mediciones del tiempo transcurrido en las ejecuciones
+        de think())
+        :return:
+        '''
+        return self._think_time
+
+    @property
+    def update_time(self):
+        '''
+        Esta propiedad devuelve una estimación del tiempo necesario para ejecutar el método update() de este
+        controlador (se devuelve una media de las últimas mediciones del tiempo transcurrido en las ejecuciones
+        de update())
+        :return:
+        '''
+        return self._update_time
+    
